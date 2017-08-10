2965c4d4af9475cbce62a5d34b8456ae859f1443
Bob Ballantyne <bob.ballantyne@emc.com>
2017-08-09 21:09:02 -0400

    add code to update plugins.txt

---
+++ container/latestPlugins.groovy
@@ -0,0 +1,41 @@
+import groovy.json.*
+
+def isUpdated = 0
+
+def f = new File('plugins.txt')
+def pluginList = [:] 
+f.readLines().each { line ->
+    def details = line.split(':')
+    pluginList[details[0]] = details[1]
+}
+
+f = new URL('http://mirrors.jenkins-ci.org/updates/update-center.json')
+def jsonText = f.text
+jsonText = jsonText.substring('updateCenter.post('.length())
+jsonText = jsonText.substring(0, jsonText.length()-3)
+
+def slurper = new JsonSlurper()
+def json = slurper.parseText(jsonText)
+json.plugins.each { k,v ->
+    if (pluginList.containsKey(k) && pluginList[k] != v.version) {
+        println k+' updated from '+pluginList[k] + ' to '+ v.version
+        pluginList[k] = v.version
+        isUpdated++
+    }
+}
+
+if (isUpdated == 0) {
+    println 'No plugins updated'
+}
+else {
+    def fmt = new java.text.SimpleDateFormat("yyyyymmdd_hhmmss")
+    f = new File('plugins.txt')
+    f = f.renameTo( new File('plugins.' + fmt.format(Calendar.instance.time) + '.txt') )
+    f = new File('plugins.txt')
+    pluginList.each { k,v ->
+        f << k + ':' + v + "\n"
+    }
+    f = null
+    println isUpdated+' plugins updated'
+}
+''
