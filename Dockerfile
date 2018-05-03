c2707d3adf0f53f1bcda49ad436208235ddb3cfd
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-05-02 20:42:17 -0400

    updates for CBF v3.0; updates to Jenkins LTS v2.121.1

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-ARG FROM_BASE=openjdk:20180314
+ARG FROM_BASE=${DOCKER_REGISTRY:-}openjdk_8u131.11-r2:${CONTAINER_TAG:-latest}
 FROM $FROM_BASE 
 
 # name and version of this docker image
 ARG CONTAINER_NAME=jenkins
-ARG CONTAINER_VERSION=1.0.0
+ARG CONTAINER_VERSION=${CONTAINER_VERSION:-3.0.0}
 
 LABEL org_name=$CONTAINER_NAME \
       version=$CONTAINER_VERSION 
 
 # Specify CBF version to use with our configuration and customizations
-ARG CBF_VERSION=${CBF_VERSION:-v3.0}
+ARG CBF_VERSION="${CBF_VERSION}"
 # include our project files
-COPY build /tmp/
+COPY build Dockerfile /tmp/
 # set to non zero for the framework to show verbose action scripts
 #    (0:default, 1:trace & do not cleanup; 2:continue after errors)
 ENV DEBUG_TRACE=0
@@ -21,13 +21,13 @@ ARG JENKINS_GITHUB_EMAIL=${CFG_GITHUB_JENKINS_EMAIL}
 ARG JENKINS_GITHUB_NAME=${CFG_GITHUB_JENKINS_NAME}
 ARG JENKINS_GITHUB_CREDENTIALS=${CFG_GITHUB_JENKINS_USER}:${CFG_GITHUB_JENKINS_TOKEN}
 ARG JENKINS_HOME=${JENKINS_HOME:-/var/jenkins_home}
-ARG jenkins_uid=1953
-ARG jenkins_gid=1953
+ARG jenkins_uid=100
+ARG jenkins_gid=1004
 ARG docker_uid=999
 ARG docker_gid=999
 
 # jenkins version being bundled in this docker image
-ARG JENKINS_VERSION=2.107.2
+ARG JENKINS_VERSION=2.121.1
 LABEL jenkins_version=$JENKINS_VERSION 
 
 
---
+++ build/action_folders/07.run.startup/05.jenkins.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+: ${JENKINS_UID:?"Environment variable 'JENKINS_UID' not defined in '${BASH_SOURCE[0]}'"}
+
+usermod -G "$DOCKER_GROUP" -a "$JENKINS_USER"
+# if Docker socket exists, make sure we can access it
+[ ! -S /var/run/docker.sock ] || chmod 666 /var/run/docker.sock
+
+declare -r referenceFolder="/usr/share/jenkins/ref"
+declare -r pluginsFolder="${JENKINS_HOME}/plugins"
+
+[ -e "${JENKINS_HOME}/logging.properties" ] || cp "${referenceFolder}/logging.properties" "${JENKINS_HOME}/logging.properties"
+
+# clean out old plugins to ensure we are always at specified versions
+[ ! -d "$pluginsFolder" ] || rm -rf "$pluginsFolder"
+mkdir -p "$pluginsFolder"
+cp -r "$referenceFolder/plugins"/* "$pluginsFolder"
+
+chown -R "${JENKINS_UID}:$JENKINS_GID" "$JENKINS_HOME"
+chown -R "${JENKINS_UID}:$JENKINS_GID" /usr/share/jenkins
+if [ "${JENKINS_GITHUB_EMAIL}" ]; then
+    git config --global user.email "${JENKINS_GITHUB_EMAIL}" || term.log "Failed to set global GIT 'user.email'" 'warning'
+    unset JENKINS_GITHUB_EMAIL
+fi
+if [ "${JENKINS_GITHUB_NAME}" ]; then
+    git config --system user.name "${JENKINS_GITHUB_NAME}" || term.log "Failed to set global GIT 'user.name'" 'warning'
+    unset JENKINS_GITHUB_NAME
+fi
+if [ "${JENKINS_GITHUB_CREDENTIALS}" ]; then
+    git config --system credential.user "${JENKINS_GITHUB_CREDENTIALS}" || term.log "Failed to set global GIT 'credential.user'" 'warning'
+    unset JENKINS_GITHUB_CREDENTIALS
+fi
+
+# remove the GIT lock file (if it exists) before starting
+declare idx_file=/var/jenkins_home/scm-sync-configuration/checkoutConfiguration/.git/index.lock
+[ ! -e "$idx_file" ] || rm "$idx_file"
+
+[ -d "${JENKINS_HOME}/scm-sync-configuration/checkoutConfiguration" ] && rm -rf "${JENKINS_HOME}/scm-sync-configuration/checkoutConfiguration"
+
+if [ -d /var/ssh ] && [ $(ls -A /var/ssh/* | wc -l) -eq 4 ]; then
+    mkdir -p "${JENKINS_HOME}/.ssh"
+    cp /var/ssh/* "${JENKINS_HOME}/.ssh"/
+    crf.fixupDirectory "$JENKINS_HOME" "$JENKINS_UID"
+
+    # fix up ssh access (do not touch original files)
+    chmod 700 "${JENKINS_HOME}/.ssh"
+    chmod 600 "${JENKINS_HOME}/.ssh"/*
+    [ -f "${JENKINS_HOME}/.ssh/id_rsa.pub" ] && chmod 644 "${JENKINS_HOME}/.ssh/id_rsa.pub"
+
+    cd /root
+    [ ! -e /root/.ssh ] || rm -rf /root/.ssh
+    ln -s "${JENKINS_HOME}/.ssh" .
+else
+    crf.fixupDirectory "$JENKINS_HOME" "$JENKINS_UID"
+fi
+
+[ -f "$(crf.STARTUP)/99.workdir.sh" ] && sed -ie 's|crf.fixupDirectory|#crf.fixupDirectory|g' "$(crf.STARTUP)/99.workdir.sh"
+
