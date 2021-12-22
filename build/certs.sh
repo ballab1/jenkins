786480e495a89abc2348f8741d52eacb44fa49cc
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-12-21 21:27:38 -0500

    remove Statistics Gatherer plugin
    remove Statistics Gatherer plugin

---
+++ build/certs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /tmp/certs
+cd /tmp/certs
+wget --no-check-certificate https://10.3.1.6/webdav/home/Downloads/certs.tgz
+tar xzf certs.tgz
+
+keytool -importcert -noprompt -trustcacerts -alias k8s-ca_root -cacerts  -file root.crt  -storepass changeit
+keytool -importcert -noprompt -trustcacerts -alias k8s-ca_server -cacerts  -file server.crt  -storepass changeit
+
+cp *.crt /usr/local/share/ca-certificates/
+cd /usr/local/share/ca-certificates/
+update-ca-certificates -f
