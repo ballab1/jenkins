a35f34c656032926b0e2274352c6f65ef0375390
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-05-10 19:48:06 -0400

    update Jenkins to v2.504.1

--- build/certs.sh
+++ build/certs.sh
@@ -5,9 +5,10 @@ cd /tmp/certs
 wget --no-check-certificate https://10.3.1.10/webdav/home/Downloads/certs.tgz
 tar xzf certs.tgz
 
-keytool -importcert -noprompt -trustcacerts -alias k8s-ca_root -cacerts  -file root.crt  -storepass changeit
-keytool -importcert -noprompt -trustcacerts -alias k8s-ca_server -cacerts  -file server.crt  -storepass changeit
+for file in SohoBall_CA.crt SohoBall-Server.crt; do
+  keytool -importcert -noprompt -trustcacerts -alias k8s-ca_root -cacerts  -file "$file"  -storepass changeit
+  cp "$file" /usr/local/share/ca-certificates/
+done
 
-cp *.crt /usr/local/share/ca-certificates/
 cd /usr/local/share/ca-certificates/
 update-ca-certificates -f
