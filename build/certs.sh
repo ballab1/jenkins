204e9a0a0edbbfba851647bcb42d2ec6446a7c5d
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-06-22 17:45:53 -0400

    update jenkins to version 2.504.2

--- build/certs.sh
+++ build/certs.sh
@@ -4,6 +4,7 @@ mkdir -p /tmp/certs
 cd /tmp/certs
 wget --no-check-certificate https://10.3.1.10/webdav/home/Downloads/certs.tgz
 tar xzf certs.tgz
+#tar xzf ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz
 
 for file in SohoBall_CA.crt SohoBall-Server.crt; do
   keytool -importcert -noprompt -trustcacerts -alias k8s-ca_root -cacerts  -file "$file"  -storepass changeit
